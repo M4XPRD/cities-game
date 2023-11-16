b91be44e461ce619ef1c0ec123998972bda54e12
Max <ppmax@bk.ru>
2023-11-16 22:39:50 +0300

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
