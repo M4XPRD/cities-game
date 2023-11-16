460e8424700fe7382759a9e1245b2040d73a01b0
Max <ppmax@bk.ru>
2023-11-16 22:42:52 +0300

    Main component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DescriptionCard from './components/DescriptionCard';
+import './styles/index.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <main className="bg-gray-200 h-screen flex justify-center items-center font-sans">
+    <DescriptionCard />
+  </main>
 );
 
 export default App;
