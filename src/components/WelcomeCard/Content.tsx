c91d1dd4a10eff4fb2b3134cb0ca00b7fee0cc04
Max <ppmax@bk.ru>
2023-11-17 17:53:33 +0300

    A small refactoring of WelcomeCard content

--- src/components/WelcomeCard/Content.tsx
+++ src/components/WelcomeCard/Content.tsx
@@ -1,24 +1,12 @@
 /* eslint-disable max-len */
 import useNavigation from '../../hooks/navigationHook';
-import { GameRules } from '../../types/gameRules';
-
-const rules: GameRules[] = [
-  { key: 'first', text: 'Запрещается повторение городов.' },
-  {
-    key: 'second',
-    text: 'Названий городов на твердый “ъ” и мягкий “ъ” знак нет. Из-за этого бы пропускаем эту букву и игрок должен назвать город на букву стоящую перед ъ или ь знаком.',
-  },
-  {
-    key: 'third',
-    text: 'Каждому игроку дается 2 минуты на размышления, если спустя это время игрок не вводит слово он считается проигравшим.',
-  },
-];
+import rules from '../../utils/gameRules';
 
 const Content = () => {
   const { handleNextPage } = useNavigation();
 
   return (
-    <div className="h-[283px] bg-white rounded-b-[16px] prose-sm">
+    <div className="h-[283px] bg-white rounded-b-[16px] prose-sm shadow-md">
       <section className="flex flex-col justify-between p-[24px] h-[235px] text-gray-700">
         <div>Цель: Назвать как можно больше реальных городов.</div>
         <ul>
