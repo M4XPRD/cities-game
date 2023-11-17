1aaf54b8cac121a552561237b4a4e8b2aed8e448
Max <ppmax@bk.ru>
2023-11-17 17:53:02 +0300

    Game logic provider added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import NavigationProvider from './contexts/NavigationProvider';
 import './styles/index.css';
+import GameLogicProvider from './contexts/GameLogicProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <NavigationProvider>
-      <App />
+      <GameLogicProvider>
+        <App />
+      </GameLogicProvider>
     </NavigationProvider>
   </React.StrictMode>,
 );
