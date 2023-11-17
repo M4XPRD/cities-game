0809cf8224143d682faf234bccf987656772b69d
Max <ppmax@bk.ru>
2023-11-17 12:14:01 +0300

    Components mapping added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import DescriptionCard from './components/DescriptionCard';
+import EndgameCard from './components/EndgameCard/EndgameCard';
+import GameCard from './components/GameCard/GameCard';
+import DescriptionCard from './components/WelcomeCard/DescriptionCard';
+import useNavigation from './hooks/navigationHook';
 import './styles/index.css';
+import { PagesMappingTypes } from './types/pagesMappingTypes';
 
-const App = () => (
-  <main className="bg-gray-200 h-screen flex justify-center items-center font-sans">
-    <DescriptionCard />
-  </main>
-);
+const App = () => {
+  const { page } = useNavigation();
+
+  const pagesMapping: PagesMappingTypes = {
+    1: <DescriptionCard />,
+    2: <GameCard />,
+    3: <EndgameCard />,
+  };
+
+  return (
+    <main className="bg-gray-200 h-screen flex justify-center items-center font-sans">
+      {pagesMapping[page]}
+    </main>
+  );
+};
 
 export default App;
