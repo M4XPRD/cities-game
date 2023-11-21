5caaf4b80f84a5977a44fbf2d864ce3579ca38a4
Max <ppmax@bk.ru>
2023-11-21 20:47:24 +0300

    First page HTML fixes

--- src/components/WelcomeCard/Content.tsx
+++ src/components/WelcomeCard/Content.tsx
@@ -6,21 +6,25 @@ const Content = () => {
   const { handleNextPage } = useNavigation();
 
   return (
-    <div className="h-[283px] bg-white rounded-b-[16px] prose-sm shadow-md">
-      <section className="flex flex-col justify-between p-[24px] h-[235px] text-gray-700">
-        <div>Цель: Назвать как можно больше реальных городов.</div>
-        <ul>
-          {rules.map(({ key, text }) => (
-            <li key={key} className="m-0">{text}</li>
-          ))}
-        </ul>
-        <button
-          type="button"
-          className="bg-violet-600 w-[126px] min-h-[40px] rounded-[4px] text-white self-center prose-base font-medium"
-          onClick={handleNextPage}
-        >
-          Начать игру
-        </button>
+    <div className="flex flex-col justify-center items-center h-[283px] bg-white rounded-b-[16px] prose-sm shadow-md">
+      <section className="flex flex-col justify-evenly pt-[24px] pl-[24px] pr-[24px] h-[235px] text-gray-700 text-sm">
+        <div className="flex flex-col gap-[24px] justify-start h-inherit">
+          <div>Цель: Назвать как можно больше реальных городов.</div>
+          <ul className="m-0">
+            {rules.map(({ key, text }) => (
+              <li key={key} className="m-0 text-sm">
+                {text}
+              </li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            className="bg-violet-600 w-[126px] min-h-[40px] rounded-[4px] text-white self-center prose-base font-medium text-base mb-[24px]"
+            onClick={handleNextPage}
+          >
+            Начать игру
+          </button>
+        </div>
       </section>
     </div>
   );
--- src/components/WelcomeCard/Header.tsx
+++ src/components/WelcomeCard/Header.tsx
@@ -1,5 +1,5 @@
 const Header = () => (
-  <h1 className="text-center bg-white rounded-t-[16px] h-[64px] border-b-[3px] border-gray-100 pt-[17px] prose-base">
+  <h1 className="text-center bg-white rounded-t-[16px] h-[64px] border-b-[3px] border-gray-100 pt-[17px] prose-base text-base">
     Игра в города на время
   </h1>
 );
